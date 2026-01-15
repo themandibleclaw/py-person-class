a8169cb009fd4d9e219cfcf58ecaf874c6250846
claw <themandibleclaw@gmail.com>
2026-01-15 22:02:37 +0000

    implement requirements

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 class Person:
-    # write your code here
-    pass
+    people = dict()
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for person in people:
+        new_person = Person(person["name"], person["age"])
+        Person.people[new_person.name] = new_person
+
+    person_list = []
+    for person in people:
+        lookup = Person.people[person["name"]]
+        if person.get("wife") is not None:
+            wife = Person.people[person["wife"]]
+            lookup.wife = wife
+        if person.get("husband") is not None:
+            husband = Person.people[person["husband"]]
+            lookup.husband = husband
+        person_list.append(lookup)
+    return person_list
